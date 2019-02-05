34437c59d2eae3ea6c4b9ff774490c54e18b0ee5
HalforcNull <yaorunan@gmail.com>
2019-02-05 14:29:19 -0600

    more test

--- HelloShiny/customPackages/test2.R
+++ HelloShiny/customPackages/test2.R
@@ -1,6 +1,25 @@
 library(shiny)
 library(shinyBS)
 library(DT)
+library("rhdf5")
+
+library('R6')
+
+Ctl.LoadData <- R6Class("Ctl.LoadData")
+
+Ctl.LoadData$set(  
+  "public",
+  "datasetInfo",
+  function(){
+    reactive({
+      dataset.info <- read.table("D:/iDep Data/data/readCounts/GSEinfo.txt", sep="\t",header=T )
+      dataset.info$GEO.ID = as.character(dataset.info$GEO.ID)
+      return(dataset.info)
+    })
+  }
+)
+
+
 
   ui <- 
     fluidPage(
@@ -30,53 +49,56 @@ library(DT)
                   	
 			),
 			bsModal(
-				"modalSearchTab", 
-				"Public RNA-Seq and ChIP-Seq data", 
-				"tabBut2", 
-				size="large",
-				fluidPage(
-					h5("Download gene-level read counts data for 7,793 human and mouse datasets from",
-						a("ARCHS4 ", href="http://amp.pharm.mssm.edu/archs4/help.html"),
-						"on Nov. 5, 2018.",
-						"HiSeq 2000 and HiSeq 2500 raw data from NCBI's SRA database are 
-						aligned with Kallisto against human GRCh38 or mouse GRCm38 cDNA reference."
-					),
-					fluidRow( 
-						column(	9, 
-								h5("Search and click on a dataset to see more information before download.") 
-						),
-            	       column(	3, 
-								selectInput(
-									"selected.species.archs4", 
-									"", 
-            	        			choices = list("Human"= "human", "Mouse" = "mouse"), 
-									selected = "human"
-								)
-						)
-					),
-					DT::dataTableOutput('SearchData'),
-					HTML('<hr style="height:1px;border:none;color:#333;background-color:#333;" />'),
-					br(),
-					fluidRow( 	
-						column(4, 	textOutput('selectedDataset') ),
-	                   	column(3, 	downloadButton('downloadSearchedData', 'Download') ),
-	                   	column(5, 	h5(	"Edit and uplodad file to ", 
-										a("iDEP", href="http://bioinformatics.sdstate.edu/idep/"), 
-										"for analysis"
-									)
-						)	
-	         		),
-					br(),
-					tableOutput('samples'),
-					h4("Loading data and R packages ... ..."),
-					htmlOutput('DoneLoading') 
-				)	
-			)
+			  "modalSearchTab", 
+			  "Public RNA-Seq and ChIP-Seq data", 
+			  "tabBut2", 
+			  size="large",
+			  fluidPage(
+			    h5("Download gene-level read counts data for 7,793 human and mouse datasets from",
+			       a("ARCHS4 ", href="http://amp.pharm.mssm.edu/archs4/help.html"),
+			       "on Nov. 5, 2018.",
+			       "HiSeq 2000 and HiSeq 2500 raw data from NCBI's SRA database are 
+			       aligned with Kallisto against human GRCh38 or mouse GRCm38 cDNA reference."
+			    ),
+			    fluidRow( 
+			      column(	9, 
+			              h5("Search and click on a dataset to see more information before download.") 
+			      ),
+			      column(	3, 
+			              selectInput(
+			                "selected.species.archs4", 
+			                "", 
+			                choices = list("Human"= "human", "Mouse" = "mouse"), 
+			                selected = "human"
+			              )
+			      )
+			    ),
+			    DT::dataTableOutput('SearchData'),
+			    HTML('<hr style="height:1px;border:none;color:#333;background-color:#333;" />'),
+			    br(),
+			    fluidRow( 	
+			      column(4, 	textOutput('selectedDataset') ),
+			      column(3, 	actionButton('downloadSearchedData', 'Download') ),
+			      column(5, 	h5(	"Edit and uplodad file to ", 
+			                     a("iDEP", href="http://bioinformatics.sdstate.edu/idep/"), 
+			                     "for analysis"
+			      )
+			      )	
+			    ),
+			    br(),
+			    tableOutput('samples'),
+			    h4("Loading data and R packages ... ..."),
+			    htmlOutput('DoneLoading') 
+			    )	
+        )
+			
+			
         )
       )
     )
   
   server <- function(input, output, session) {
+      myTestCtrl <- Ctl.LoadData$new()
       
       output$distPlot <- renderPlot({
 
@@ -114,11 +136,7 @@ library(DT)
           handleUploadIdChanged()
       })
       
-      dataset.info <- reactive({
-        dataset.info <- read.table(GSEInfoFile, sep="\t",header=T )
-        dataset.info$GEO.ID = as.character(dataset.info$GEO.ID)
-        return(dataset.info)
-      })
+      dataset.info <- myTestCtrl$datasetInfo()
       
       # retrieve sample info and counts data
       Search <- reactive({
